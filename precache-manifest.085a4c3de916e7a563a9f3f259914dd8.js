30006ebe693bcfeca03331da9666bf1003f7bf83
Olga Zwijacz <olgazwijacz@gmail.com>
2019-08-25 14:53:19 +0200

    Updates

---
+++ precache-manifest.085a4c3de916e7a563a9f3f259914dd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed1d9db04a3162f3e34c88995b3ed8d0",
+    "url": "/beers/index.html"
+  },
+  {
+    "revision": "84862faa9ca4ea61cdf7",
+    "url": "/beers/static/css/2.d4b275d9.chunk.css"
+  },
+  {
+    "revision": "351fb0742698ff9882cc",
+    "url": "/beers/static/css/main.507336ad.chunk.css"
+  },
+  {
+    "revision": "84862faa9ca4ea61cdf7",
+    "url": "/beers/static/js/2.b5aab586.chunk.js"
+  },
+  {
+    "revision": "351fb0742698ff9882cc",
+    "url": "/beers/static/js/main.45e54af0.chunk.js"
+  },
+  {
+    "revision": "a8f5e4c2eefd62c2b6f3",
+    "url": "/beers/static/js/runtime~main.7d526466.js"
+  }
+]);
